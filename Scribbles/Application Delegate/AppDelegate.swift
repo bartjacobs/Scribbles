2001a0e358457912f04b0bc723ec847cc47bcb69
Bart Jacobs <git@codefoundry.be>
2017-10-13 11:18:37 +0200

    Project setup

---
+++ Scribbles/Application Delegate/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Scribbles
+//
+//  Created by Bart Jacobs on 13/10/2017.
+//  Copyright © 2017 Cocoacasts. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Properties
+
+    var window: UIWindow?
+
+    // MARK: - Application Life Cycle
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
---
+++ Scribbles/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RootViewController.swift
+//  Scribbles
+//
+//  Created by Bart Jacobs on 13/10/2017.
+//  Copyright © 2017 Cocoacasts. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+}
