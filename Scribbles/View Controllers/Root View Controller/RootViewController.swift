499481c184fd5be12a93b2cbcb08c15850abf00c
Bart Jacobs <git@codefoundry.be>
2017-10-19 11:44:22 +0200

    Reflect account status changes in user interface

--- Scribbles/View Controllers/Root View Controller/RootViewController.swift
+++ Scribbles/View Controllers/Root View Controller/RootViewController.swift
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class RootViewController: UIViewController {
 
     // MARK: - Properties
 
+    @IBOutlet var accountStatusLabel: UILabel!
+
+    // MARK: -
+
     private let cloudKitManager = CloudKitManager()
+
+    // MARK: -
+
+    private let disposeBag = DisposeBag()
 
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Subscribe to Account Status
+        cloudKitManager
+            .accountStatus
+            .asDriver(onErrorJustReturn: .couldNotDetermine)
+            .map { (accountStatus) -> String in
+                switch accountStatus {
+                case .couldNotDetermine: return "Unable to Determine iCloud Account Status"
+                case .available: return "User Signed in to iCloud"
+                case .restricted: return "Not Permitted to Access iCloud Account"
+                case .noAccount: return "User Not Signed in to iCloud"
+                }
+            }
+            .drive(accountStatusLabel.rx.text)
+            .disposed(by: disposeBag)
     }
 
 }
